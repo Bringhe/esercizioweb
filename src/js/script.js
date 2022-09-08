6695114c43ed9a28d6f118c6a424e90d8afa5787
Bringhe <a.bringhenti@itsrizzoli.it>
2022-09-08 12:08:59 +0200

    aggiunto file script.js

---
+++ src/js/script.js
@@ -0,0 +1,5 @@
+console.log('ci siamoo');
+const btn=document.querySelector(".btn-open-menu");
+const body=document.querySelector("body");
+btn.addEventListener("click", ()=>{body.classList.toggle("open-menu");
+});
